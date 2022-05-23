ae642f78d8ac050f0e7a8654020bc5e599c395fd
pPlanello <planello96@gmail.com>
2022-05-23 19:42:51 +0200

    Create structure server

---
+++ models/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.middlewares();
+
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use( express.static('public'))
+    }
+
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.send('Hello World');
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running at port: ', this.port);
+        });
+    }
+}
+
+module.exports = Server;
