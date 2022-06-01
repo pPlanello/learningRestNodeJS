32a9154177cabaae1485c422fc99fa6614b31a04
pPlanello <planello96@gmail.com>
2022-06-01 20:18:15 +0200

    Implements fields validations

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -28,10 +28,17 @@ const createUser = async (req = request, res = response) => {
 
     const {username, email, password, rol} = req.body;
     const user = new User({username, email, password, rol});
+
     // Verify email
+    const existEmail = await User.findOne({email});
+    if (existEmail) {
+        return res.status(404).json({msg: `The email '${email}' already exist`});
+    }
+
     // Encrypt password
     const salt = bcrypt.genSaltSync();
     user.password = bcrypt.hashSync(password, salt);
+
     // Save
     await user.save();
 
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getUsers, createUser, updateUser, deleteUser } = require('../controllers/users.controller');
+const { validFields } = require('../middlewares/valid-fields');
 
 const router = Router();
 
@@ -7,8 +9,15 @@ router.get('/', getUsers);
 
 router.put('/:id', updateUser);
 
-router.post('/', createUser);
+router.post('/', [ 
+        check('email', 'The email is invalid').isEmail(),
+        check('username', 'The username is mandatory').not().isEmpty(),
+        check('password', 'The password must be more than 6 letters.').isLength({min: 6}),
+        check('rol', 'The rol value is not valid').isIn(['ADMIN_ROL', 'USER_ROL']),
+        validFields
+    ],
+    createUser);
 
-router.delete('/', deleteUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
