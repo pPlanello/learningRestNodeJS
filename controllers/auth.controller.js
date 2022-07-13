c1abf481caf3c1861d5a904292768422b5062ead
pPlanello <planello96@gmail.com>
2022-07-13 19:08:32 +0200

    Create jwt token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,46 @@
 const { response } = require("express");
+const bcrypt = require('bcryptjs/dist/bcrypt');
 
-const login = (req, res = response) => {
+const User = require("../models/user");
+const {generateJWT} = require('../utils/generate-jwt');
+
+const login = async (req, res = response) => {
+    const { email, password } = req.body;
+    
+    var token = '';
+
+    try {
+        // Verify email
+        const user = await User.findOne({email});
+        // Verify password
+        const validPassword = user ? bcrypt.compareSync(password, user.password) : false;
+
+        if (!user || !validPassword) {
+            return res.status(400).json({
+                msg: 'Email or password incorrect.'
+            });
+        }
+
+        // User active
+        if (!user.state) {
+            return res.status(400).json({
+                msg: 'Email is not active.'
+            });
+        }
+
+        // Generate JWT
+        token = await generateJWT(user.id);
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            msg: 'A problem has occurred, please contact with the administrator.'
+        })
+    }
+
     res.json({
-        msg: 'Login OK'
+        msg: 'Login successfully',
+        token
     })
 }
 
---
+++ utils/generate-jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const generateJWT = (uid = '') => {
+    return new Promise((resolve, reject) => {
+        const payload = {uid};
+        jwt.sign(payload, process.env.SECRET_PRIVATE_KEY, {
+            expiresIn: '2h'
+        }, (error, token) => {
+            if (error) {
+               console.error(error);
+               reject('It can not be possible to generate token');
+            }
+            resolve(token);
+        });
+    });
+}
+
+module.exports = {
+    generateJWT
+}
